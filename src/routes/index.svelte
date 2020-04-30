db3b6ceea1566e1f37d2a69ddb226f0b5694a6f5
Amar Gill <amar.s.gill@outlook.com>
2020-04-30 01:27:11 -0400

    initial commit.

---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+<style>
+
+</style>
+
+<svelte:head>
+	<title>Resume</title>
+</svelte:head>
+
+<h2>Amar Singh Gill - Full Stack Developer</h2>
+<p><strong>Email</strong> <a href="mailto:amar.s.gill@outlook.com" alt="email">amar.s.gill@outlook.com</a></p>
+<p><strong>Github</strong> <a href="https://github.com/Amar-Gill" alt="Github" target="_blank">Amar-Gill</a></p>
+<p><strong>LinkedIn</strong> <a href="https://www.linkedin.com/in/amar-s-gill-65860662/" alt="LinkedIn" target="_blank">Amar Gill</a></p>
+
+<h2>Skills</h2>
+<p><strong>JavaScript</strong> React, Jest, Node, Express, JQuery, Typescript, Svelte, Sapper</p>
+<p><strong>Python</strong> Flask, Anaconda3, Pytorch, iPython, Jupyter Notebook, Spyder, Kivy</p>
+<p><strong>Mobile Development</strong> Flutter, Dart</p>
+<p><strong>Development Methodologies</strong> Test Driven Development, Mobile Responsiveness, RESTful APIs, Agile, RabbitMQ message brokering</p>
+<p><strong>Databases</strong> PostgreSQL, MongoDB, Cloud Firestore</p>
+<p><strong>Cloud Infrastructure</strong> Heroku, AWS S3, Firebase</p>
+<p><strong>eCommerce</strong> Braintree, SendGrid</p>
+
+<h2>Work History</h2>
+<p><strong>Morrison Hershfield Ltd. (2017-2019)</strong> Building science consulting and contract administration services for building owners, operators, product manufacturers and regulatory compliance (LEED, Passive House)</p>
+<p><strong>Voith Hydro GmbH (2016-2017)</strong> Leading scrum meetings, budget estimation, submittals tracking</p>
+<p><strong>Ontario Power Generation (2014-2015)</strong> Project management oversight for a $3 million domestic waterline replacement project</p>
+<h2>Personal Projects</h2>
+<p><strong>Leet Quiz [Flutter, Firebase, Firestore]</strong></p>
+<ul>
+	<li>A mobile quiz game where users answer questions on a variety of topics related to web and mobile development</li>
+	<li>Implemented a Firestore NoSQL database to save user progress and Firebase authentication for Google and Apple accounts</li>
+	<li>Available for download in <a href="https://www.apple.com/ios/app-store/" alt="apple app store" target="_blank"> Apple App Store</a> and <a href="https://play.google.com/store/apps/details?id=dev.amar.quizdemo" alt="play store" target="_blank">Google Play Store</a></li>
+</ul>
+<p><strong>Site Reporting App [React, Flask, PostgreSQL, AWS S3]</strong></p>
+<ul>
+	<li>Actively developing a quality assurance platform for professionals that perform construction site visits inspired by my experience as a consultant</li>
+	<li>User interface built as a single page application in React using SemanticUI CSS library to achieve clean and minimal design</li>
+	<li>Integrated action tracking into reporting workflow for better user experience when conducting site visits for quality assurance purposes</li>
+	<li>Enabled AWS S3 bucket storage for user uploaded images</li>
+</ul>
+<p><strong>Habit Wars [Flask, PostgreSQL, AWS S3, RabbitMQ, Heroku]</strong></p>
+<ul>
+	<li>Competitive habit tracking game built as final project for the Full Stack Web Development bootcamp at NEXT Academy (Full Stack Web Development Bootcamp in Kuala Lumpur, Malaysia)</li>
+	<li>Implemented chronological jobs using RabbitMQ to end active game rounds and start new rounds each week</li>
+	<li>Voted ‘Most Creative App’ by audience at final project showcase</li>
+</ul>
---
+++ src/routes/privacy_policy.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Privacy Policy</title>
+</svelte:head>
+
+<h2>Personal Privacy Policy for Apps Developed by Amar S. Gill</h2>
+<p>Applications distributed through publicly available channels may collect email
+	addresses. This is to enable authentication of users of the application and for gathering usage
+	statistics for product development purposes. User data will remain anonymous and
+	will not be sold to third parties for the purposes of marketing or advertising. </p>
+<h2>Contact</h2>
+<p>For questions please reach out to:</p>
+<ul>
+	<li><a href="mailto:amar.s.gill@outlook.com" alt="email">amar.s.gill@outlook.com</a></li>
+</ul>
