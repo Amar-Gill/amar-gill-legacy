c135391af6387ecc756ba4375561625a9b76bd8a
Amar Gill <amar.s.gill@outlook.com>
2020-09-17 13:48:40 -0400

    refactored Menu component to use details element instead of custom dropdown container class. component functionality achieved with maps interaction and highlighting.

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,177 +1,137 @@
-<div
- in:fly="{{x:-200, duration: 800}}"
- class="menu-container">
+<script>
+  import { fly } from "svelte/transition";
+  import { onMount, createEventDispatcher } from "svelte";
 
-    <button class="dropdown-btn">Power Generation
-        <i class="fa fa-caret-down"></i>
-    </button>
+  let activeProjectTitle; // used to style currently selected project
+  let marker;
 
-    <div class="dropdown-container" >
-        {#each energyProjects as eProject, i}
-            <p aria-current="{activeProjectTitle === eProject.title ? true : undefined}" on:click="{() => handleClick(eProject)}" >
-                {i + 1}: {eProject.title}
-            </p>
-        {/each}
-    </div>
-    
-    <button class="dropdown-btn">Building Envelope
-        <i class="fa fa-caret-down"></i>
-    </button>
+  export let map;
+  export let beProjects;
+  export let energyProjects;
+  export let sustainabilityProjects;
 
-    <div class="dropdown-container" >
-        {#each beProjects as beProject, i}
-            <p aria-current="{activeProjectTitle === beProject.title ? true : undefined}" on:click="{() => handleClick(beProject)}" >
-                {i + 1}: {beProject.title}
-            </p>
-        {/each}
-    </div>
+  const dispatch = createEventDispatcher();
 
-    <button class="dropdown-btn">Green Buildings
-        <i class="fa fa-caret-down"></i>
-    </button>
+  function handleClick(project) {
+    map.panTo(project.center);
+    map.setZoom(17);
 
-    <div class="dropdown-container" >
-        {#each sustainabilityProjects as sProject, i}
-            <p aria-current="{activeProjectTitle === sProject.title ? true : undefined}" on:click="{() => handleClick(sProject)}" >
-                {i + 1}: {sProject.title}
-            </p>
-        {/each}
-    </div>
+    // add marker
+    if (!marker) {
+      marker = new google.maps.Marker({
+        map,
+        position: project.center
+      });
+    } else {
+      marker.setPosition(project.center);
+    }
 
-</div>
+    dispatch("select", {
+      project: project
+    });
 
-<!-- use tutorial for drawer menu -->
-<!-- https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp -->
+    // set active project to the clicked project to activate highlighting
+    activeProjectTitle = project.title;
+  }
+</script>
 
 <style>
-    .menu-container {
-        z-index: 2;
-        top: 9%;
-        position: fixed;
-        display: flex;
-        flex-direction: column;
-        width: 16rem;
-        left: 1%;
-        background-color: var(--hue-1);
-        border-radius: 9px;
-        box-shadow: .5rem .5rem var(--compliment-1);
-        color: var(--compliment-2);
-    }
+  .menu-container {
+    z-index: 2;
+    top: 9%;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    width: 16rem;
+    left: 1%;
+    background-color: var(--hue-1);
+    border-radius: 9px;
+    box-shadow: 0.5rem 0.5rem var(--compliment-1);
+    color: var(--compliment-2);
+  }
 
+  summary {
+      cursor: pointer;
+  }
+
+  @media (min-width: 451px) and (max-width: 835px) {
     .dropdown-btn {
-        text-decoration: none;
-        font-size: 1.5rem;
-        display: block;
-        border: none;
-        background: none;
-        width: 100%;
-        text-align: left;
-        cursor: pointer;
-        outline: none;
-        margin-bottom: 1rem;
-        color: inherit;
+      font-size: 1.25rem;
     }
 
-    @media (min-width: 451px) and (max-width: 835px) {
-        .dropdown-btn {
-            font-size: 1.25rem;
-        }
+    .menu-container {
+      width: 14rem;
+    }
+  }
 
-        .menu-container {
-            width: 14rem;
-        }
+  @media (max-width: 450px) {
+    .dropdown-btn {
+      font-size: 1rem;
     }
 
-    @media (max-width: 450px) {
-        .dropdown-btn {
-            font-size: 1rem;
-        }
-
-        .menu-container {
-           width: 12rem;
-        }
+    .menu-container {
+      width: 12rem;
     }
+  }
 
-    .dropdown-container {
-        display: none;
-        border-radius: 9px;
-        margin: 0;
-    }
+  .dropdown-container {
+    display: none;
+    border-radius: 9px;
+    margin: 0;
+  }
 
-    p {
-        margin: 0 0 .5rem 2rem;
-        cursor: pointer;
-    }
+  p {
+    /* margin: 0 0 0.5rem 2rem; */
+    cursor: pointer;
+  }
 
-    p:hover{
-        color: var(--compliment-1);
-    }
+  p:hover {
+    color: var(--compliment-1);
+  }
 
-    .fa-caret-down {
-        float: right;
-    }
+  [aria-current] {
+    color: var(--hue-3);
+    font-weight: 600;
+  }
 
-    [aria-current] {
-        color: var(--hue-3);
-        font-weight: 600;
-    }
-
-    [aria-current]:hover {
-        color: var(--hue-3);
-    }
+  [aria-current]:hover {
+    color: var(--hue-3);
+  }
 </style>
 
-<script>
-    import { fly } from 'svelte/transition';
-    import { onMount, createEventDispatcher } from 'svelte';
+<div in:fly={{ x: -200, duration: 800 }} class="menu-container">
 
-    let activeProjectTitle; // used to style currently selected project
-    let marker;
+  <details>
+    <summary>Power Generation</summary>
+    {#each energyProjects as eProject, i}
+      <p
+        aria-current={activeProjectTitle === eProject.title ? true : undefined}
+        on:click={() => handleClick(eProject)}>
+        {i + 1}: {eProject.title}
+      </p>
+    {/each}
+  </details>
 
-    export let map;
-    export let beProjects;
-    export let energyProjects;
-    export let sustainabilityProjects;
+  <details>
+    <summary>Building Envelope</summary>
+    {#each beProjects as beProject, i}
+      <p
+        aria-current={activeProjectTitle === beProject.title ? true : undefined}
+        on:click={() => handleClick(beProject)}>
+        {i + 1}: {beProject.title}
+      </p>
+    {/each}
+  </details>
 
-    const dispatch = createEventDispatcher();
+  <details>
+    <summary>Green Buildings</summary>
+    {#each sustainabilityProjects as sProject, i}
+      <p
+        aria-current={activeProjectTitle === sProject.title ? true : undefined}
+        on:click={() => handleClick(sProject)}>
+        {i + 1}: {sProject.title}
+      </p>
+    {/each}
+  </details>
 
-    function handleClick(project) {
-
-        map.panTo(project.center);
-        map.setZoom(17);
-
-        // add marker
-        if (!marker) {
-            marker = new google.maps.Marker({
-                map,
-                position: project.center
-            })
-        } else {
-            marker.setPosition(project.center);
-        }
-
-        dispatch("select", {
-            project: project
-        })
-
-        activeProjectTitle = project.title;
-    }
-
-    onMount(() => {
-        // CODE FOR BINDING DROPDOWN DRAWER BEHAVIOR TO MENU BUTTONS
-        let dropdown = document.getElementsByClassName("dropdown-btn");
-
-        for (let i = 0; i < dropdown.length; i++) {
-            dropdown[i].addEventListener("click", function () {
-                // this.classList.toggle("active");
-                var dropdownContent = this.nextElementSibling;
-                if (dropdownContent.style.display === "block") {
-                    dropdownContent.style.display = "none";
-                } else {
-                    dropdownContent.style.display = "block";
-                }
-            });
-        }
-    })
-
-</script>
+</div>
